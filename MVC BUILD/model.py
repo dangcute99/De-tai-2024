60c32a5c36f047fb35baa74826ed079b066e2c3a
dangcute99 <nguyendang992002@gmail.com>
2025-02-06 08:26:29 +0700

    delete don't use file

--- MVC BUILD/model.py
+++ MVC BUILD/model.py
@@ -164,7 +164,7 @@ class Model:
 
 # db.close_connection()
 # ###########################################################################################
-# db_config = {'host': 'localhost', 'username': 'root', 'password': '992002'}
-# model = Model(db_config)
-# model.user_exists(
-#     'login_in4', {'username': 'admin', 'passwords': '123456'})
+db_config = {'host': 'localhost', 'username': 'root', 'password': '992002'}
+model = Model(db_config)
+model.user_exists(
+    'login_in4', {'username': 'admin', 'passwords': '1234567'})
--- MVC BUILD/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-db_config = {'host': 'localhost', 'username': 'root', 'password': '992002'}
-# model = Model(db_config)
-# model.user_exists(
-#     'login_in4', {'username': 'admin', 'passwords': '123456'})
